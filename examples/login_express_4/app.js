e3bc217b07f2b2dec8fae34eea05c34dca838c25
Marcus Leite <leite.marcus@gmail.com>
2017-07-30 17:26:46 -0300

    Changes to migrate to Express 4
    Added the packages that were removed from Express core. 
    Changed how the "app" variable is initialized.
    Changes were based on the official Node.js foundation recommendations to migrate to Express 4, found on https://expressjs.com/en/guide/migrating-4.html

--- examples/login_express_4/app.js
+++ examples/login_express_4/app.js
@@ -1,4 +1,9 @@
 var express = require('express')
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , session = require('express-session')
+  , methodOverride = require('method-override')
   , passport = require('passport')
   , util = require('util')
   , StravaStrategy = require('passport-strava-oauth2').Strategy;
@@ -48,24 +53,22 @@ passport.use(new StravaStrategy({
 
 
 
-var app = express.createServer();
+var app = express();
 
 // configure Express
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.logger());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.session({ secret: 'keyboard cat' }));
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(methodOverride());
+app.use(session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res){
